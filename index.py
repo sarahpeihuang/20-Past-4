e3dcadb5eae07f15d9565f5d79601fb671eb4db3
sarah888 <shuan392@uwo.ca>
2023-02-18 20:24:05 -0500

    takes input

--- index.py
+++ index.py
@@ -1,9 +1,10 @@
-import pygame, time
+import pygame, sys
 from pygame import mixer
 
 
 pygame.init()
 mixer.init()
+clock = pygame.time.Clock()
 
 #initialize the screen
 width = 1200
@@ -11,6 +12,7 @@ height = 600
 screen = pygame.display.set_mode((width, height))
 screen_rect = screen.get_rect()
 
+#initialize images
 blk_img = pygame.image.load('black.jpg')
 blk_img = pygame.transform.scale(blk_img,(width,height))
 blk_rect = blk_img.get_rect()
@@ -35,9 +37,20 @@ washroom_img = pygame.image.load('washroom.jpg')
 washroom_img = pygame.transform.scale(washroom_img,(width,height))
 washroom_rect = washroom_img.get_rect()
 
+#initialize music
 mixer.music.load('intro.mp3')
 mixer.music.set_volume(1)
 mixer.music.play()
+
+#initialize text
+base_font = pygame.font.Font(None, 32)
+user_text = ''
+input_rect = pygame.Rect(200, 200, 140, 32)
+color_active = pygame.Color('lightskyblue3')
+color_passive = pygame.Color('chartreuse4')
+color = color_passive
+active = False
+
 running = True
 washroom = False
 party = False
@@ -55,32 +68,72 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: #quit the game
             running = False
-        elif event.type == pygame.KEYDOWN:   
-            if event.key == pygame.K_1 and washroom: #level one: washroom
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if input_rect.collidepoint(event.pos):
+                active = True
+            else:
+                active = False
+        if event.type == pygame.KEYDOWN:   
+            if event.key == pygame.K_UP and washroom: #level one: washroom
                level1 = True
                level2 = False
                level3 = False
                home = False
                party = True #makes the enxt level avaliable
-            if event.key == pygame.K_2 and party: #level 2: party
+            if event.key == pygame.K_LEFT and party: #level 2: party
                level1 = False
                level2 = True
                level3 = False
                home = False   
                classroom = True #makes the final level avaliable
-            if event.key == pygame.K_3 and classroom: #level 3: classroom   
+            if event.key == pygame.K_RIGHT and classroom: #level 3: classroom   
                level1 = False
                level2 = False
                level3 = True 
                home = False   
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_RSHIFT or event.key == pygame.K_LSHIFT: #landing page
                 washroom = True
                 home = True
                 level1 = False
                 level2 = False
                 level3 = False    
+            # Check for backspace
+            if event.key == pygame.K_BACKSPACE:
+  
+                # get text input from 0 to -1 i.e. end.
+                user_text = user_text[:-1]
+  
+            # Unicode standard is used for string
+            # formation
+            else:
+                user_text += event.unicode
+                #print("+++++++"+str(user_text))
+
+            if event.key == pygame.K_RETURN:
+                print(str(user_text))
+                user_text = ''
+
+            
 
     #game logic
+    if active:
+        color = color_active
+    else:
+        color = color_passive
+
+    # draw rectangle and argument passed which should be on screen
+    pygame.draw.rect(screen, color, input_rect)
+    text_surface = base_font.render(user_text, True, (255, 255, 255))
+      
+    # render at position stated in arguments
+    screen.blit(text_surface, (input_rect.x+5, input_rect.y+5))
+      
+    # set width of textfield so that text cannot get outside of user's text input
+    input_rect.w = max(100, text_surface.get_width()+10)
+      
+    pygame.display.flip() # display.flip() will update only a portion of the screen to updated, not full area
+    clock.tick(60) # clock.tick(60) means that for every second at most 60 frames should be passed.
+
     if washroom == False:
         while True:
             screen.blit(blk_img,(0,j))
